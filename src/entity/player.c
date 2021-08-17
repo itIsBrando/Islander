f389fec40c7a7362b2780c5c2efb97094be091c6
itIsBrando <mooballygoobally@gmail.com>
2021-08-16 23:48:04 -0400

    Implemented mining

--- src/entity/player.c
+++ src/entity/player.c
@@ -1,9 +1,11 @@
 #include <gb/gb.h>
+#include <string.h>
 
 #include "player.h"
 #include "../main.h"
 #include "../oam.h"
 #include "../item/item.h"
+#include "../item/goal.h"
 #include "../world/tile.h"
 #include "../world/chunk.h"
 #include "../world/collision.h"
@@ -17,12 +19,19 @@ player_t player;
 uint8_t cur_id, cur_x, cur_y;
 uint8_t anim_counter = 0;
 
+// @todo implement tile destruction
+uint8_t tile_damage = 0; // amount of damage for the attacking tile
+
 /**
  * Initializes player sprite and variables
  */
 void plr_init()
 {
     player.id = spr_allocate();
+
+    // initialize inventory
+    memset(&player.inventory, 255, sizeof(inv_t));
+    player.inventory.size = 0;
 
     move_sprite(player.id, PLR_SCRN_X, PLR_SCRN_Y);
     set_sprite_tile(player.id, 2);
@@ -34,7 +43,9 @@ void plr_init()
     set_sprite_tile(cur_id = spr_allocate(), 16);
 
     item_t pick = {.id = ITEM_STONE_PICK};
+    item_t wood = {.id = ITEM_WOOD, 2};
     itm_add_to_inventory(&player.inventory, &pick);
+    itm_add_to_inventory(&player.inventory, &wood);
 
     hud_draw_hotbar(&player);
 }
@@ -47,6 +58,11 @@ void plr_init()
 void plr_update(const uint8_t j)
 {
     if(j & J_B) {
+        char buf[20];
+        goal_generate_name(goal_get_current(), buf, 20);
+
+        print_window(buf, 0, 0);
+
         if(j & J_RIGHT)
             hud_move_cur(DIRECTION_RIGHT);
         else if (j & J_LEFT)
@@ -79,14 +95,14 @@ void plr_update(const uint8_t j)
     }
 
     if(j & J_LEFT) {
-        if((player.x & 63) == 0)
+        if(((player.x - 1) & 63) == 0)
             cnk_load_row(DIRECTION_LEFT);
 
         plr_move(DIRECTION_LEFT);
     }
     
     if(j & J_RIGHT) {
-        if((player.x & 63) == 0)
+        if(((player.x + 1) & 63) == 0)
             cnk_load_row(DIRECTION_RIGHT);
 
         plr_move(DIRECTION_RIGHT);
@@ -94,17 +110,66 @@ void plr_update(const uint8_t j)
 
     if(j & J_A)
     {
-        u8 x = player.x + cur_x - 4, y = player.y + cur_y - 10;
-        tile_t *t = map_get_tile_relative(x, y);
-
-        if(*t == TILE_WATER) {
-            cnk_active_write(x >> 3, y >> 3, TILE_BRIDGE);
-        }
-
-        shake(5);
-        waitjoypad(J_A);
+        plr_interact();
     }
 
+}
+
+
+/**
+ * Removes an item from the player's inventory. This function
+ *  calls `itm_sub_from_inventory` but also invokes necesssary
+ *  drawing functions for the hotbar
+ */
+void plr_sub_from_inventory(const uint8_t id, const int8_t count)
+{
+    item_t *item = itm_lookup(&player.inventory, id);
+
+    if(itm_sub_from_inventory(&player.inventory, id, count)) {
+        hud_remove_item();
+        item->id = 0xFF; // remove item from recognizable ids
+    }
+}
+
+
+/**
+ * @returns returns a pointer to the player active item
+ */
+inline item_t *plr_get_active_item()
+{
+    return &player.inventory.items[hud_cur];
+}
+
+
+/**
+ * @todo add proper item interaction
+ */
+inline void plr_interact()
+{
+    u8 x = player.x + cur_x - 4, y = player.y + cur_y - 10;
+    tile_t *t = map_get_tile_relative(x, y);
+    item_t *item = plr_get_active_item();
+    tile_data_t *tile_data = tile_data_lookup(*t);
+
+    if(ITEM_LOOKUP(item).is_tool && tile_data->mineable) {
+        shake(5);
+        tile_damage += itm_get_tile_damage(item);
+
+        // @todo improve versitility
+        if(tile_damage >= tile_data->hp) {
+            cnk_active_write(x >> 3, y >> 3, TILE_GROUND);
+            itm_add_id_to_inventory(&player.inventory, tile_data->item_id);
+            /** add optional events for tiles to
+             *   allow for trees to be mined correctly
+             *   and for other unnecessary functionality
+             */
+        }
+    } else if(itm_can_interact(item, *t)) {
+        cnk_active_write(x >> 3, y >> 3, TILE_BRIDGE);
+        plr_sub_from_inventory(plr_get_active_item()->id, 1);
+    }
+
+    waitjoypad(J_A);
 }
 
 
@@ -141,9 +206,10 @@ void plr_move(const direction_t dir)
         scroll_bkg(dx, dy);
 
         // @todo improve perfomance
-        if((anim_counter++ & 3) == 3)
-        {
+        if((anim_counter++ & 3) == 3) {
             set_sprite_tile(player.id, TILE_PLAYER_BASE + ((anim_counter & 0b1100) % 3));
         }
+
+        tile_damage = 0;
     }
 }
